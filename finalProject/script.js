aa3a3428f762798d9d5d474e19aae1a693abc0a7
FullySend <118204527+FullySend@users.noreply.github.com>
2022-12-05 14:00:52 -0500

    Update script.js

--- finalProject/script.js
+++ finalProject/script.js
@@ -19,28 +19,27 @@ inout1.on('keyup', function () {
 
 
 //Project 2 Code:
-let rangevalue = document.getElementById("myRange");
+window.addEventListener("load", function () {
 let setvalue = document.getElementById("rangevalue");
-let imgchangevalue = docuemnt.getElementById("imgchange");
+let imgchangevalue = document.getElementById("imgchange");
+let rangeValue = document.getElementById("myRange");
 
-rangevalue.oninput = function() {
-    let getvalue = rangevalue.ariaValueNow;
+rangeValue.addEventListener("mouseup", function() {
+    console.log("input works")
+    let getvalue = rangeValue.value;
     setvalue.innerHTML = this.value;
     if (getvalue > 0 && getvalue < 16){
-        imgchangevalue.setAttribute("src", "https://images.unsplash.com/photo-1666720192286-9a09913ee6cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2942&q=80")
+        imgchangevalue.setAttribute("src", "20%_image.jpeg")
     } else if (getvalue >= 17 && getvalue <= 33) {
-        imgchangevalue.setAttribute("src", "https://images.unsplash.com/photo-1666649013395-b10759afa8b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80")
+        imgchangevalue.setAttribute("src", "40% image.jpeg")
     } else if (getvalue >= 34 && getvalue <= 49) { 
-        imgchangevalue.setAttribute("src", "https://images.unsplash.com/photo-1666620657207-c748f7600a42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80") 
+        imgchangevalue.setAttribute("src", "60% image.jpeg") 
     } else if (getvalue >= 50 && getvalue <= 66) { 
-        imgchangevalue.setAttribute("src", "https://images.unsplash.com/photo-1666636413703-961f0076185d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80") 
+        imgchangevalue.setAttribute("src", "80% image.jpeg") 
     } else if (getvalue >= 67 && getvalue <= 83) { 
-        imgchangevalue.setAttribute("src", "https://images.unsplash.com/photo-1666760012395-c63bf13fb188?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1894&q=80") 
-    } else if (getvalue >= 84 && getvalue <= 99) { 
-        imgchangevalue.setAttribute("src", "https://images.unsplash.com/photo-1666791065993-f5109d7dd8d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80") 
-    } else if (getvalue >= 100) { 
-        imgchangevalue.setAttribute("src", "https://images.unsplash.com/photo-1666473574287-0424cc6d0946?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80") }
-}
-
-
+        imgchangevalue.setAttribute("src", "100% image.jpeg") 
+    } else if (getvalue >= 84 && getvalue <= 100) { 
+        imgchangevalue.setAttribute("src", "https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80") 
+    }
+    })})
 //Project 3 Code:
